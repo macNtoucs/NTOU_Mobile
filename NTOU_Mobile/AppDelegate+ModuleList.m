eed08a54a2063184104536e52b6fdf696c30d529
macNtoucs <mac.ntoucs@gmail.com>
2013-03-08 12:17:41 +0800

    新增校園導覽功能

--- NTOU_Mobile/AppDelegate+ModuleList.m
+++ NTOU_Mobile/AppDelegate+ModuleList.m
@@ -10,6 +10,7 @@
 #import "NTOUModule.h"
 #import "ShuttleModule.h"
 #import "EmergencyModule.h"
+#import	"CMModule.h"
 /*#import "NewsModule.h"
 
 #import "StellarModule.h"
@@ -47,7 +48,7 @@
    // [result addObject:[[[NewsModule alloc] init] autorelease]];
     //[result addObject:[[[StellarModule alloc] init] autorelease]];
     [result addObject:[[[ShuttleModule alloc] init] autorelease]];
-	//[result addObject:[[[CMModule alloc] init] autorelease]];
+	[result addObject:[[[CMModule alloc] init] autorelease]];
     //[result addObject:[[[CalendarModule alloc] init] autorelease]];
 	//[result addObject:[[[PeopleModule alloc] init] autorelease]];
     // [result addObject:[[[ToursModule alloc] init] autorelease]];
