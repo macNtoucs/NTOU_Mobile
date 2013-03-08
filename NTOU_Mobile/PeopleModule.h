847e3f35f146901d2c81165f8c13abf39266b037
macNtoucs <mac.ntoucs@gmail.com>
2013-03-08 11:26:47 +0800

    加入生活圈入口

---
+++ NTOU_Mobile/PeopleModule.h
@@ -0,0 +1,12 @@
+#import "NTOUModule.h"
+
+@class PeopleSearchViewController;
+
+@interface PeopleModule : NTOUModule {
+	
+	NTOU_LifeViewController *viewController;
+
+}
+
+@end
+
---
+++ NTOU_Mobile/PeopleModule.m
@@ -0,0 +1,44 @@
+#import "PeopleModule.h"
+
+
+#import "NTOUModule+Protected.h"
+
+
+@interface PeopleModule ()
+@end
+
+@implementation PeopleModule
+
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        
+        self.shortName = @"生活圈";
+        self.longName = @"People Directory";
+        self.iconName = @"people";
+    }
+    return self;
+}
+
+- (void)loadModuleHomeController
+{
+    UIStoryboard* storyboard = [UIStoryboard storyboardWithName:@"MainStoryboard" bundle:nil];
+    UIViewController* initialHelpView = [storyboard instantiateInitialViewController];
+    initialHelpView.title = @"海大生活圈";
+    //NTOU_LifeViewController *controller = [[[NTOU_LifeViewController alloc] init] autorelease];
+    self.moduleHomeController = initialHelpView;
+}
+
+
+
+- (void)dealloc
+{
+    [NTOU_LifeViewController release];
+	[super dealloc];
+}
+
+
+@end
+
